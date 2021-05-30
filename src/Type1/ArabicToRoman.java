e48ed179ed552509532527096f3e0f459c289610
sayantan.biswas <sayantan.biswas@Notebook2339.highradius.com>
2021-05-30 22:09:17 +0530

    push 2

--- src/Type1/ArabicToRoman.java
+++ src/Type1/ArabicToRoman.java
@@ -10,12 +10,13 @@ public class ArabicToRoman {
 	
 	public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        
         int l = arabic.length;
         System.out.print("Enter number of Test Cases: ");
         int t = sc.nextInt();
         System.out.print("Enter the numbers (must be <4000): ");
+        
         while(t-- > 0) {
-        	
             int n = sc.nextInt();
             if(n>3999) {
             	System.out.println("The entered number must be < 4000. Try Again!!");
@@ -31,6 +32,7 @@ public class ArabicToRoman {
 	private static String convertToRoman(int n, int l) {
 		String result = "";
 		int ct = 1;
+		
 		while(n > 0) {
             for(int i=0; i<l; i++) {
                 if(n >= arabic[i]) {
@@ -39,6 +41,7 @@ public class ArabicToRoman {
                         result = result.replace("i", roman[i]);
                     else
                         result += roman[i];
+                    
                     int size = result.length();
                     if(size>1) {
                         if(result.charAt(size-1) == result.charAt(size-2))
@@ -49,6 +52,7 @@ public class ArabicToRoman {
                     if(ct>3) {
                         result = result.substring(0, size-ct) + "i" + roman[i-1];
                         int ind = result.indexOf("i");
+                        
                         if(i-2>=0 && ind>0 && (result.charAt(ind-1) == result.charAt(ind+1))) {
                             char c = result.charAt(ind-1);
                             result = result.replace(c+"i"+c, "i"+roman[i-2]);

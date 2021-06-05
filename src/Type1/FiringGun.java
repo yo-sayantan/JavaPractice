ffe5a750844db2898f81e9b187133662b2d7b036
sayantan.biswas <sayantan.biswas@highradius.com>
2021-06-05 16:59:27 +0530

    Push 1.3

---
+++ src/Type1/FiringGun.java
@@ -0,0 +1,36 @@
+package Type1;
+
+import java.util.Scanner;
+
+public class FiringGun {
+
+	 public static void main(String args[]) throws InterruptedException {
+	        Scanner sc = new Scanner(System.in);
+	        int stock, magazine = 20, speed = 100, reloadTime = 3000;
+	        System.out.print("Ente magazine stock: ");
+	        stock = sc.nextInt();
+	        
+	        while(stock-- > 1) {
+	            fire(magazine, speed);
+	            reload(reloadTime);
+	        }
+	        fire(magazine, speed);
+	        sc.close();
+	    }
+	    
+	    public static void fire(int magazine, int speed) throws InterruptedException {
+	        int count = 0;
+	        while(magazine-- > 0) {
+	            System.out.println("Firing bullet " + ++count);
+	            Thread.sleep(speed);
+	        }
+	    }
+	    
+	    public static void reload(int reloadTime) throws InterruptedException {
+	        while(reloadTime > 0) {
+	            System.out.println("Reloading...");
+	            Thread.sleep(reloadTime);
+	            reloadTime -= 500;
+	        }
+	    }
+}

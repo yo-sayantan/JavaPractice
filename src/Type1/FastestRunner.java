833c801b1a3c296f8af796154db3303ee7770b3e
sayantan.biswas <sayantan.biswas@highradius.com>
2021-06-09 14:35:16 +0530

    Push 1.5, 1.6

---
+++ src/Type1/FastestRunner.java
@@ -0,0 +1,81 @@
+package Type1;
+
+import java.util.Scanner;
+
+/**
+ * All runners should not have same running time
+ * Runners running time should not be <8 or >12
+ * Not more than 10 Test cases
+ * Prints the name having least running time
+ * Input = [6
+			Robert,9.50
+			Peter,9.58
+			Richard,9.48
+			Ronald,9.59
+			Simon,9.54
+			Williams,9.52]
+ * Output = Richard
+ * @author sayantan.biswas
+ *
+ */
+public class FastestRunner {
+	public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of runners = ");
+        int t = sc.nextInt();
+        String[] arr = new String[t];
+        
+        if(t>10)
+            System.out.println(t + " is an invalid number of runners");
+        else {
+        	System.out.println("Enter data: ");
+            for(int i=0; i<t; i++)
+                arr[i] = sc.next();
+        
+            if(check(arr))
+                return;
+            else
+                System.out.println(runner(arr));
+        }
+        
+        sc.close();
+    }
+	
+	public static String runner(String[] arr) {
+        String name = arr[0].split(",")[0];
+        float min = Float.parseFloat(arr[0].split(",")[1]);
+        
+        for(String s : arr) {
+            String[] x = s.split(",");
+            float time = Float.parseFloat(x[1]);
+
+            if(time < min) {
+                min = time;
+                name = x[0];
+            }
+        }
+        System.out.print("Fastest runner is : ");
+        return name;
+    }
+    
+    public static boolean check(String[] arr) {
+        float ch = Float.parseFloat(arr[0].split(",")[1]);
+        int ct = 0;
+        
+        for(String s : arr) {
+            float x = Float.parseFloat(s.split(",")[1]);
+            if(x > 12 && x < 8) {
+                System.out.println(x + " is an invalid input");
+                return true;
+            }
+            
+            if(x == ch)
+                ct++;
+            else 
+                return false;
+        }
+        System.out.println(arr.length + " runners have same timing");
+        return true;
+    }
+
+}
---
+++ src/Type1/ReverseAndDelete.java
@@ -0,0 +1,64 @@
+package Type1;
+
+import java.util.Scanner;
+
+/**
+ * Reverse the number and delete the middle digit
+ * No of test cases must be >0 and <20
+ * The numbers must have 3 digits [100-999]
+ * Input = [5
+			156
+			197
+			365
+			789
+			106]
+ * Output = [61
+			97
+			53
+			71
+			61]
+ * @author sayantan.biswas
+ *
+ */
+public class ReverseAndDelete {
+	public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter number of numbers = ");
+        int t = sc.nextInt();
+        int i=0,p=t;
+        
+        int arr[] = new int[t];
+        if(t<=0)
+            System.out.println(t + " is too small");
+        else if(t>20)
+            System.out.println(t + " exceeds the limit");
+        else {
+        	System.out.println("Enter the numbers: ");
+            while(t-- > 0) {
+                int x = sc.nextInt();
+                if(x >= 100 && x <= 999)
+                    arr[t] = x;
+                else {
+                    System.out.println(x + " is not in range");
+                    break;
+                }
+            }
+            System.out.println("The output is : ");
+            for(i=0; i<p; i++)
+                System.out.println(calc(arr[i], 3));
+        }
+        
+        sc.close();
+	}
+	
+	public static String calc(int n, int l) {
+        StringBuilder ans = new StringBuilder();
+        String num = String.valueOf(n);
+        String rev = "" + num.charAt(0) + num.charAt(2);
+        
+        ans.append(rev);
+        ans.reverse();
+        return ans.toString();
+    }
+}
